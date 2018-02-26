ec9d47f19029d6bd108298590263fc4854c80787
arturo <informa.fj@gmail.com>
2018-02-26 10:05:42 -0300

    cambios en el seed para actualizar imagenes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,27 +7,33 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Product.destroy_all
+# Product.destroy_all
 
 urls = ['technics', 'business', 'sports', 'transport' ]
 # urls = ['technics' ]
 
 
-30.times do
-	Product.create(
-		code: Faker::Commerce.promotion_code,
-		name: Faker::Commerce.product_name,
-		description: Faker::Lorem.paragraphs(1),
-		price: Faker::Commerce.price.to_i,
-		stock: Random.rand(60...100),
-		stock_minimum: Random.rand(20...30),
-		tax: [0,5,10].sample,
-		# remote_image_url: "http://placeimg.com/640/480/tech"
-		# remote_image_url: "https://lorempixel.com/500/400/#{urls.sample}/"
-		remote_image_url: Faker::LoremPixel.image("500x400", false, urls.sample)
-	)
-end
+# 30.times do
+# 	Product.create(
+# 		code: Faker::Commerce.promotion_code,
+# 		name: Faker::Commerce.product_name,
+# 		description: Faker::Lorem.paragraphs(1),
+# 		price: Faker::Commerce.price.to_i,
+# 		stock: Random.rand(60...100),
+# 		stock_minimum: Random.rand(20...30),
+# 		tax: [0,5,10].sample,
+# 		# remote_image_url: "http://placeimg.com/640/480/tech"
+# 		# remote_image_url: "https://lorempixel.com/500/400/#{urls.sample}/"
+# 		remote_image_url: Faker::LoremPixel.image("500x400", false, urls.sample)
+# 	)
+# end
 
+u = Product.all
+u.each do |i|
+	i.image = "https://lorempixel.com/500/400/#{urls.sample}/"
+	i.name = Faker::Commerce.product_name
+	i.save
+end
 
 # Product.all.each do
 # 	Product.update(name: Faker::Commerce.product_name, remote_image_url: "http://placeimg.com/640/480/tech")
